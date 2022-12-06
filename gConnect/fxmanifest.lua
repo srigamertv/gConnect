7fd9a656cd87fffd8d06da6f5177fef3ea49ec70
SR.IGAMER TV <108888437+srigamertv@users.noreply.github.com>
2022-12-06 13:27:55 -0300

    up

---
+++ gConnect/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "gConnect"
+description "scritp Webhook connect mostrar players online"
+author "SR.IGAMER TV"
+version "1.0.0"
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
---
+++ gConnect/shared/main.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.Debug = false -- mandar mensagem para CMD? true = SIM
+
+Config.Webhook = '' -- copie e cole sua webhook
+Config.ServerName = 'CANAL SR.IGAMER TV ' -- Titulo da Msg
+
+Config.MessageId = '' -- inicie o resource sem ID, após iniciar, copie o id da mensagem e cole aqui.
+
+Config.UpdateTime = 1 -- tempo para atualizar (em minutos)
+Config.Use24hClock = true -- true= 24h / false= 12h (am/pm)
+Config.Connect = 'SR.IGAMER TV' -- connect da cidade
+
+Config.EmbedColor = 8586501
+Config.WebhookIcon = 'https://media.discordapp.net/attachments/1027741113844502560/1028191930917597194/LOGO_COM_FUNDO.png?width=685&height=683'
