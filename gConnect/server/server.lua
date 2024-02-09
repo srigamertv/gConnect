15fe02073b790c8fac44eaaa01b4fb7aeb1d96b1
SR.IGAMER TV <108888437+srigamertv@users.noreply.github.com>
2024-02-09 20:50:28 -0300

    atualização no Config e server coloquei link no config , e true e false no pra aparecer os links connectlink, LOJA, REDES,

--- gConnect/server/server.lua
+++ gConnect/server/server.lua
@@ -1,64 +1,113 @@
 Citizen.CreateThread(function()
     while true do
-	if Config.MessageId ~= nil and Config.MessageId ~= '' then
-		UpdateStatusMessage()
-	else
-		DeployStatusMessage()
-		break
-	end
+        if Config.MessageId ~= nil and Config.MessageId ~= '' then
+            UpdateStatusMessage()
+        else
+            DeployStatusMessage()
+            break
+        end
 
-	Citizen.Wait(60000*Config.UpdateTime)
+        Citizen.Wait(60000*Config.UpdateTime)
     end
 end)
 
 function DeployStatusMessage()
-	local footer = nil
+    local footer = nil
 
-	if Config.Use24hClock then
-		footer = os.date('Data: %d/%m/%Y  |  Hora: %H:%M')
-	else
-		footer = os.date('Data: %d/%m/%Y  |  Hora: %I:%M %p')
-	end
+    if Config.Use24hClock then
+        footer = os.date('Data: %d/%m/%Y  |  Hora: %H:%M:%S')
+    else
+        footer = os.date('Data: %d/%m/%Y  |  Hora: %I:%M:%S %p')
+    end
 
-	if Config.Debug then
-		print('Deplying Status Message ['..footer..']')
-	end
+    if Config.Debug then
+        print('Deploying Status Message ['..footer..']')
+    end
 
-	local embed = {
-		{
-			["color"] = Config.EmbedColor,
-			["title"] = "**Implantando mensagem de status**",
-			["description"] = 'Copie o ID desta mensagem e coloque-o em Config e reinicie o script!',
-			["footer"] = {
-				["text"] = footer,
-			},
-		}
-	}
+    local embed = {
+        {
+            ["color"] = Config.EmbedColor,
+            ["title"] = "**Implantando mensagem de status**",
+            ["description"] = 'Copie o ID desta mensagem e coloque-o em Config e reinicie o script!',
+            ["footer"] = {
+                ["text"] = footer,
+            },
+        }
+    }
 
-	PerformHttpRequest(Config.Webhook, function(err, text, headers) end, 'POST', json.encode({
-		embeds = embed, 
-	}), { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest(Config.Webhook, function(err, text, headers) end, 'POST', json.encode({
+        embeds = embed, 
+    }), { ['Content-Type'] = 'application/json' })
 end
 
 function UpdateStatusMessage()
-	local players = #GetPlayers()
-	local maxplayers = GetConvarInt('sv_maxclients', 0)
-	local footer = nil
-	local minutos = Config.UpdateTime
-	local connect = Config.Connect
+    local players = #GetPlayers()
+    local maxplayers = GetConvarInt('sv_maxclients', 0)
+    local footer = nil
+    local minutos = Config.UpdateTime
+    local connect = Config.Connect
+    local fivem = Config.emojifivem  -- Emoji que será exibido como o botão (altere conforme desejado)
+    local emoji = Config.emojiloja -- Emoji que será exibido como o botão (altere conforme desejado)
+    local redes = Config.emojirede -- Emoji que será exibido como o botão (altere conforme desejado)
 
 	if Config.Use24hClock then
-		footer = os.date(' %d/%m/%Y | Hora: %H:%M | Atualização a cada ' ..minutos.. ' minutos')
+		footer = os.date(' %d/%m/%Y | Hora: %H:%M:%S | Atualização a cada ' ..minutos.. ' minutos')
 	else
-		footer = os.date(' %d/%m/%Y | Hora: %I:%M %p | Atualização a cada ' ..minutos.. ' minutos')
+		footer = os.date(' %d/%m/%Y | Hora: %I:%M:%S %p | Atualização a cada ' ..minutos.. ' minutos')
 	end
 
-	if Config.Debug then
-		print('Atualizando mensagem de status ['..footer..']')
-	end
-	local fivem = "`🎮          `" -- Emoji que será exibido como o botão (altere conforme desejado)
-    local emoji = "`     🛒     `" -- Emoji que será exibido como o botão (altere conforme desejado)
-	local tiktok = "`          🔈`" -- Emoji que será exibido como o botão (altere conforme desejado)
+    if Config.Debug then
+        print('Atualizando mensagem de status ['..footer..']')
+    end
+
+    local fields = {
+        {
+            ["name"]= "> 🕹️  Players",
+            ["value"]= '```\n ['..players..' / '..maxplayers..'] ```',
+            ["inline"]= true
+        },
+        {
+            ["name"]= "> 📡  Status",
+            ["value"]= '```\n [ONLINE]```',
+            ["inline"]= true
+        },
+        {
+            ["name"]= "> Connect FiveM",
+            ["value"]= '```connect '..connect..'```',
+        },
+        {
+            ["name"] = "\n",
+            ["value"] = "\n",
+        },
+        {
+            ["name"] = "\n",
+            ["value"] = "\n",
+        }
+    }
+
+    if Config.showConnectField then
+        table.insert(fields, {
+            ["name"]= "> 🎮 Connect",
+            ["value"]= ' \n ['..fivem..']('..Config.connectlink..')', -- Emoji clicável que abre o link
+            ["inline"]= true
+        })
+    end
+
+    if Config.showStoreField then
+        table.insert(fields, {
+            ["name"]= "> 🛒 Loja",
+            ["value"]= '\n ['..emoji..']('..Config.loja..')',
+            ["inline"]= true
+        })
+    end
+
+    if Config.showSocialMediaField then
+        table.insert(fields, {
+            ["name"]= "> 🔈 Redes",
+            ["value"]= '\n    ['..redes..']('..Config.redes..')',
+            ["inline"]= true
+        })
+    end
 
     local message = json.encode({
         embeds = {
@@ -71,54 +120,15 @@ function UpdateStatusMessage()
                 ["footer"] = {
                     ["text"] = footer ,
                 },
-                ["fields"]= {
-                    {
-                        ["name"]= "> 🕹️  Players",
-                        ["value"]= '```\n ['..players..' / '..maxplayers..'] ```',
-                        ["inline"]= true
-                    },
-                    {
-                        ["name"]= "> 📡  Status",
-                        ["value"]= '```\n [ONLINE]```',
-                        ["inline"]= true
-                    },
-                    {
-                        ["name"]= "> Connect FiveM",
-                        ["value"]= '```connect '..connect..'```',
-                    },
-					{
-						["name"] = "\n",
-						["value"] = "\n",
-					},
-					{
-						["name"] = "\n",
-						["value"] = "\n",
-					},
-                    {
-                        ["name"]= "> 🎮 Connect",
-                        ["value"]= ' \n ['..fivem..'](https://github.com/srigamertv/gConnect)', -- Emoji clicável que abre o link
-                        ["inline"]= true
-                    },
-                    {
-                        ["name"]= "> 🛒 Loja",
-                        ["value"]= '\n ['..emoji..'](https://github.com/srigamertv/gConnect)',
-                        ["inline"]= true
-                    },
-                    {
-                        ["name"]= "> 🔈 Tiktok",
-                        ["value"]= '\n ['..tiktok..'](https://github.com/srigamertv/gConnect)',
-                        ["inline"]= true
-                    },
-                },
+                ["fields"]= fields
             }
         }
     })
 
-
-	PerformHttpRequest(Config.Webhook..'/messages/'..Config.MessageId, function(err, text, headers) 
-		if Config.Debug then
-			print('[DEBUG] err=', err)
-			print('[DEBUG] text=', text)
-		end
-	end, 'PATCH', message, { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest(Config.Webhook..'/messages/'..Config.MessageId, function(err, text, headers) 
+        if Config.Debug then
+            print('[DEBUG] err=', err)
+            print('[DEBUG] text=', text)
+        end
+    end, 'PATCH', message, { ['Content-Type'] = 'application/json' })
 end
--- gConnect/shared/main.lua
+++ gConnect/shared/main.lua
@@ -2,14 +2,26 @@ Config = {}
 
 Config.Debug = false -- mandar mensagem para CMD? true = SIM
 
-Config.Webhook = '' -- copie e cole sua webhook
+Config.Webhook = 'https://discord.com/api/webhooks/1185262636212961461/NcK9HIb8ZTF0Vtd0I5IBRfDd20U0L1ys6g68RY07x90OTJsdDF7jkfW97qR14lhL37Rs' -- copie e cole sua webhook
 Config.ServerName = 'CANAL SR.IGAMER TV ' -- Titulo da Msg
 
-Config.MessageId = '' -- inicie o resource sem ID, após iniciar, copie o id da mensagem e cole aqui.
+Config.MessageId = '1205645450871312425' -- inicie o resource sem ID, após iniciar, copie o id da mensagem e cole aqui.
 
 Config.UpdateTime = 1 -- tempo para atualizar (em minutos)
 Config.Use24hClock = true -- true= 24h / false= 12h (am/pm)
 Config.Connect = 'SR.IGAMER TV' -- connect da cidade
+
+Config.showConnectField = true -- se for true aparece o link de connect / se for false não irá aparecer  
+Config.emojifivem = "`🎮          `"
+Config.connectlink = 'https://github.com/srigamertv/gConnect'
+
+Config.showStoreField = true -- se for true aparece o link da loja / se for false não irá aparecer
+Config.emojiloja = "`     🛒     `"
+Config.loja = 'https://github.com/srigamertv/gConnect'
+
+Config.showSocialMediaField = true -- se for true aparece o link da das redes sociais (lembrando apenas link abaixo) / se for false não irá aparecer
+Config.emojirede = "`          🔈`"
+Config.redes = 'https://github.com/srigamertv/gConnect'
 
 Config.EmbedColor = 8586501
 Config.WebhookIcon = 'https://media.discordapp.net/attachments/1027741113844502560/1028191930917597194/LOGO_COM_FUNDO.png?width=685&height=683'
